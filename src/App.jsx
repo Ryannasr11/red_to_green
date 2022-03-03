7ff71d12ff336595c2a4b3c1fc248180f0a6c810
Ryannasr11 <Ryannasr11@gmail.com>
2022-03-03 14:01:22 -0500

    circle

--- src/App.jsx
+++ src/App.jsx
@@ -7,40 +7,8 @@ import useMorse from "./useMorse";
 import logo from "./logo.png";
 import "./App.css";
 
-function App() {
-  const [text, setText] = useState("");
-  const [dot, setDot] = useState("");
-  const [dash, setDash] = useState("");
-  const [error, setError] = useState();
-  let morse = useMorse(text, dot, dash, setError);
-
-  return (
-    <div className="App">
-      <img src={logo} alt="Morsemoji!" />
-      <p className="tagline">Convert text to morse code... with emojis!</p>
-
-      <EmojiPicker dot={dot} setDot={setDot} dash={dash} setDash={setDash} />
-      <br />
-      <p>{error}</p>
-      <input
-        type="text"
-        placeholder="Text to translate"
-        value={text}
-        onChange={(event) => {
-          setText(event.target.value.toLowerCase());
-          setError(null);
-        }}
-      />
-      {!!morse.length && (
-        <div className="copychunk">
-          <p className="emojis">{morse}</p>
-          <CopyToClipboard text={morse}>
-            <button>Copy to Clipboard</button>
-          </CopyToClipboard>
-        </div>
-      )}
-    </div>
-  );
+export default function App() {
+    return (
+        <span class="dot"></span>
+    );
 }
-
-export default App;
