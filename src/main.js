97df09175e899723bac541503a482bc931efeaf5
Ryannasr11 <Ryannasr11@gmail.com>
2022-03-10 02:45:46 -0500

    please bruh

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import './App.css'
 
 document.querySelector("button").addEventListener('click', function(event) {
+    document.querySelector("button").style.backgroundColor = '#00FF00';
     event.currentTarget.setAttribute("disabled", true);
 }, {
     once: true
