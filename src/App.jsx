94d6dccbea95d76b0dbc44ee153809d487ec8214
Ryannasr11 <Ryannasr11@gmail.com>
2022-03-10 02:13:19 -0500

    kkkk

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Fab from '@mui/material/Fab';
 
-let button = document.getElementById("button");
 
-button.addEventListener("click", () => {
-    button.classList.toggle('clicked');
-})
 export default function Circle() {
+    let button = document.getElementById("button");
+
+    button.addEventListener("click", () => {
+        button.classList.toggle('clicked');
+    })
     return (
         <Grid container justify={'center'}>
             <button id="button"></button>
