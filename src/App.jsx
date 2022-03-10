1249004a7f48be2a608da50f530c43fb0bae115e
Ryannasr11 <Ryannasr11@gmail.com>
2022-03-10 02:21:56 -0500

    i hate this

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,6 @@ import Fab from '@mui/material/Fab';
 
 
 export default function Circle() {
-    let button = document.getElementById("button");
-
-    button.addEventListener("click", () => {
-        button.classList.toggle('clicked');
-    })
     return (
         <Grid container justify={'center'}>
             <button id="button"></button>
