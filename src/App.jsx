88d861023f8bce3de13aea0347fee3ecf4beefce
Ryannasr11 <Ryannasr11@gmail.com>
2022-03-10 01:46:14 -0500

    l

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,6 @@ import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Fab from '@mui/material/Fab';
 
-function changeColor() {
-    document.getElementById("a").onclick = updateCL();
-}
-
-
-
 export default function Circle() {
     let buttonOne = document.querySelector('#one');
     buttonOne.addEventListener('click', () => buttonOne.style.backgroundColor = '#00FF00')
