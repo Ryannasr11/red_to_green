d63097279a703de23094c34443273a165a922040
Ryannasr11 <Ryannasr11@gmail.com>
2022-03-10 00:05:16 -0500

    3/10 v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import React, { useState } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
-import EmojiPicker from "./EmojiPicker";
 
-import useMorse from "./useMorse";
-
-import logo from "./logo.png";
 import "./App.css";
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
