7cf2d5b0482570f25428598d35c2333f988e6ba3
Ryannasr11 <67965192+Ryannasr11@users.noreply.github.com>
2022-04-12 14:40:55 -0400

    Update timechecker.js

--- src/timechecker.js
+++ src/timechecker.js
@@ -29,7 +29,7 @@ document.querySelector("section").innerHTML = speedSensor;
 let n = 0;
 while (n < 1) {
     if (clicked = 1) {
-        speed1 = Math.floor(Math.random() * 11) + 10;
+        speed1 = Math.floor(Math.random() * 42) + 13;
         speed2 = Math.floor((Math.random() * 899) + 100);
         speedSensor = "Server Response Time: " + speed1 + "." + speed2 + "ms";
         n = 2;
