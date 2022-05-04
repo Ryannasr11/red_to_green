92b3f86ac29e5ba0a86e480ae1d7095aa9793ab5
Ryannasr11 <Ryannasr11@gmail.com>
2022-05-04 07:05:51 -0400

    bug

--- src/timechecker.js
+++ src/timechecker.js
@@ -9,7 +9,7 @@ console.log(speedSensor);
 
 
 document.querySelector("button").addEventListener('click', function(event) {
-    document.querySelector("button").style.backgroundColor = '#009900';
+    document.querySelector("button").style.backgroundColor = '#64c541';
     event.currentTarget.setAttribute("disabled", true);
     document.querySelector("section").innerHTML = speedSensor;   
 }, {
@@ -29,7 +29,7 @@ document.querySelector("section").innerHTML = speedSensor;
 let n = 0;
 while (n < 1) {
     if (clicked = 1) {
-        speed1 = Math.floor(Math.random() * 42) + 13;
+        speed1 = Math.floor(Math.random() * 10) + 10;
         speed2 = Math.floor((Math.random() * 899) + 100);
         speedSensor = "Server Response Time: " + speed1 + "." + speed2 + "ms";
         n = 2;
